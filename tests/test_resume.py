8b1e4f8982e97a1d60a5b6fb9e650eb3d70ede3e
Gregory Eberwine <geberwine@gmail.com>
2026-04-02 15:45:13 -0400

    additional tests added

--- tests/test_resume.py
+++ tests/test_resume.py
@@ -6,3 +6,18 @@ def test_has_title(page: Page):
 
     # expects title to contain this string
     expect(page).to_have_title(re.compile("Resume"))
+
+def test_email_link(page: Page):
+    page.goto("https://gregoryeberwine.com/")
+
+    # expects email link to point to my email
+    email_link = page.get_by_role("link", name="geberwine@gmail.com")
+    expect(email_link).to_have_attribute("href", "mailto:geberwine@gmail.com")
+
+def test_counter_number(page: Page):
+    page.goto("https://gregoryeberwine.com/")
+
+    # expects visitor counter to be a number
+    counter = page.locator("#visitorCounter")
+    expect(counter).not_to_have_text("")
+    expect(counter).to_have_text(re.compile(r"\d+"))
