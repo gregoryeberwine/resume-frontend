c6e7f513988bf26a94d5a4b38819c5c2226ee754
Gregory Eberwine <geberwine@gmail.com>
2026-04-02 16:21:05 -0400

    additional test added

--- tests/test_resume.py
+++ tests/test_resume.py
@@ -21,3 +21,15 @@ def test_counter_number(page: Page):
     counter = page.locator("#visitorCounter")
     expect(counter).not_to_have_text("")
     expect(counter).to_have_text(re.compile(r"\d+"))
+
+def test_counter_increments(page: Page):
+    page.goto("https://gregoryeberwine.com/")
+    
+    # expects visitor counter value to change after each reload
+    counter = page.locator("#visitorCounter")
+    expect(counter).to_have_text(re.compile(r"\d+"))
+    first_count = int(counter.text_content())
+    page.reload()
+    expect(counter).to_have_text(re.compile(r"\d+"))
+    second_count = int(counter.text_content())
+    assert second_count > first_count
