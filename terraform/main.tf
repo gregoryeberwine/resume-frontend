0a24afa59a2e6ad6eb7c5b4f93ea1ba9766caae2
Gregory Eberwine <geberwine@gmail.com>
2026-04-07 12:03:44 -0400

    add output for CF Distribtution id

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,6 @@ resource "aws_s3_bucket" "resume" {
   force_destroy    = true
 }
 
-output "s3bucketname" {
-  value = aws_s3_bucket.resume.bucket
-}
-
 data "aws_iam_policy_document" "origin_bucket_policy" {
   statement {
     sid    = "AllowCloudFrontServicePrincipal"
